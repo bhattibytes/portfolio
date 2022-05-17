cee722f2a60cf2c6d5497145a731d0649b8338f0
BhattiBytes <bhattibytes@gmail.com>
2022-05-17 14:00:03 -0400

    Freeze appbar at the top

--- components/ResponsiveAppBar.js
+++ components/ResponsiveAppBar.js
@@ -40,7 +40,7 @@ const ResponsiveAppBar = () => {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="fixed">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}  
