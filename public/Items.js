a1772fa62251e79433e125c8d257c2c8e8e8c8cd
Jason Bhatti <72463293+bhattibytes@users.noreply.github.com>
2024-02-05 17:02:54 -0500

    Update: testimonial profile pics

--- public/Items.js
+++ public/Items.js
@@ -5,49 +5,49 @@ const items = {
         "id": 1,
         "name": "Sajib Hossain",
         "title": "Full Stack Software Engineer at Flokq",
-        "imageUrl": "https://media.licdn.com/dms/image/C5603AQE7kJm4uqmr7g/profile-displayphoto-shrink_100_100/0/1658936727811?e=1706745600&v=beta&t=IJChfoQdCCRLOF3qYNoMQhpWuXtqlzAlkFujp9sAn4k",
+        "imageUrl": "https://images2.imgbox.com/59/bc/Exsh2anZ_o.jpeg",
         "testimonial": "I had the privilege of collaborating with Jason during my tenure at Charla Technologies, Inc. Throughout our work together, I found him to be consistently supportive and guided me effectively throughout my time at the company. What struck me the most was his remarkable willingness to assist junior colleagues. Jason possesses a wealth of coding experience and has a strong commitment to maintaining clean code practices. Additionally, his rapid acquisition of new technologies is truly impressive. I wholeheartedly endorse him for any organizations seeking a software engineer with exceptional leadership qualities."
       },
       {
         "id": 2,
         "name": "Jorge Soto",
         "title": "Co-Founder at AssetMule",
-        "imageUrl": "https://media.licdn.com/dms/image/C4E03AQEWihaGEIqmFA/profile-displayphoto-shrink_100_100/0/1646063041235?e=1701907200&v=beta&t=JYHRYdUKRn4dJzWpCtzvUHIFcmtUAlembKBSzuBLutE",
+        "imageUrl": "https://images2.imgbox.com/de/0d/1wRLt3By_o.jpeg",
         "testimonial": "Jason is a hard worker whose determination to succeed is matched by few. He has a great analytic mind and is able to problem solve very quickly. Jason is a great addition to our team."
       },
       {
         "id": 3,
         "name": "Aaron Bollinger",
         "title": "Entrepreneur and Sales Leader",
-        "imageUrl": "https://media.licdn.com/dms/image/C4E03AQFOzuncWuImXw/profile-displayphoto-shrink_100_100/0/1628647934831?e=1701907200&v=beta&t=mA5hdkV3e-J1dSP8kcQPScMXlUPB8V6meyuU8VCOtPs",
+        "imageUrl": "https://images2.imgbox.com/83/e5/5ziWUDGa_o.jpeg",
         "testimonial": "Jason has a genuine passion for new and innovative technology. He enjoys working with people as much as he enjoys engineering/technology, which is a great mix for someone in business development within the technology industry. Jason is a good salesman and a good guy. I recommend talking to him!"
       },
       {
         "id": 4,
         "name": "Michael Weaver",
         "title": "Chief Executive Officer at Fairchild Media",
-        "imageUrl": "https://media.licdn.com/dms/image/D4E03AQGprPtya8XnNA/profile-displayphoto-shrink_100_100/0/1691936088686?e=1706745600&v=beta&t=KKC_xOFTMfPMqgjT2djA8mPmCKBxvnlOXUXbk255Txw",
+        "imageUrl": "https://images2.imgbox.com/2a/67/UUmXt6Xs_o.jpeg",
         "testimonial": "I've worked closely with Jason the last five months and respect his problem solving ability. Jason has consistently shown the ability to take a loosely defined problem and quickly find the solution. He is a valuable part of our team and consistently gets results week after week. He has excellent customer relationship management skills. Jason is an asset to our team as he would be to any company he works for. It has been a pleasure working with Jason and I'd gladly work with him on future endeavors."
       }, 
       {
         "id": 5,
         "name": "Sean Mulry",
         "title": "Managing Partner at Target Recruiting Network",
-        "imageUrl": "https://media.licdn.com/dms/image/C4E03AQE1YzQa8HBheA/profile-displayphoto-shrink_100_100/0/1516269890543?e=1701907200&v=beta&t=MbA_Wm-ChnLR9Q0MTpX-FV2_DEHNqTZQxTJho2BLG0I",
+        "imageUrl": "https://images2.imgbox.com/76/88/6v3FuG75_o.jpeg",
         "testimonial": "I had the pleasure of both meeting and learning from Jason at the Annual Salesforce Data.com Rainmaker event in Las Vegas this year; where he was a Keynote Speaker. Jason created an amazing presentation for sales executives on how to effectively utilize social media, SEO, and content marketing solutions; which was absolutely priceless. Jason is a Subject Matter Expert who is very well versed in new technologies and has genuine + exciting passion for helping others become successful!"
       },
       {
         "id": 6,
         "name": "Kevin Gallivan",
         "title": "Account Executive at Salesforce",
-        "imageUrl": "https://media.licdn.com/dms/image/D4E03AQE3Ef7KHYnzpA/profile-displayphoto-shrink_100_100/0/1668782656423?e=1701907200&v=beta&t=LW4wcHUfIgomeAKaxKkfF5hTZcMKCtO721YTUlcJopI",
+        "imageUrl": "https://images2.imgbox.com/25/ab/qAtkR7yB_o.jpeg",
         "testimonial": "I had the pleasure of working with Jason in 2015 and I can say he is professional, a team player, and cares deeply about his clients. He has in-depth technical expertise, and I thoroughly enjoyed working with him."
       },
       {
         "id": 7,
         "name": "Jason Merhaut",
         "title": "Senior Director Business Development at Lastmile Retail",
-        "imageUrl": "https://media.licdn.com/dms/image/C4D03AQF1SfbefeyIow/profile-displayphoto-shrink_100_100/0/1599584549427?e=1701907200&v=beta&t=Kd7dg6b-86trmemUwjLSp7CVDoeJ8z9Z01tRFR7hOH0",
+        "imageUrl": "https://images2.imgbox.com/1a/5e/mWbn4xyt_o.jpeg",
         "testimonial": "Jason would be an excellent addition to any sales team from both a cultural and revenue-driving perspective. He's a fantastic team player who constantly finds new ways to address a client's needs while making sure he does what he can to assist other sales folks. He excels at finding new prospects and unearthing their contact information. And once he gets in front of a prospect, he can close with the best of them."
       }
     ]
